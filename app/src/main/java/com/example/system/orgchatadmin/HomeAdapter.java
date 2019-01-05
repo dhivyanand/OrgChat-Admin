b66d64a78da3a9e1060b550a13a5eab2345e9224
dhivyanand <dhivyananthan98@gmail.com>
2019-01-05 20:30:36 +0530

    Developed "getView()" method of HomeAdapter.

--- app/src/main/java/com/example/system/orgchatadmin/HomeAdapter.java
+++ app/src/main/java/com/example/system/orgchatadmin/HomeAdapter.java
@@ -1,14 +1,24 @@
 package com.example.system.orgchatadmin;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by System on 5/1/19.
  */
 
 public class HomeAdapter extends BaseAdapter {
+
+    Context c;
+
+    HomeAdapter(Context c){
+        this.c = c;
+    }
 
     @Override
     public int getCount() {
@@ -27,6 +37,12 @@ public class HomeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+
+        LayoutInflater layoutInflater = (LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View root = layoutInflater.inflate(R.layout.homeadapter,null);
+        ImageView icon = (ImageView)root.findViewById(R.id.icon);
+        TextView title = (TextView)root.findViewById(R.id.title);
+
+        return root;
     }
 }
