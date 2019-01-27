9ca327699e2d7877795b6c24fd867a58266b63e4
dhivyanand <dhivyananthan98@gmail.com>
2019-01-27 20:47:24 +0530

    Created activity "AddDepartment" and its corresponding resource files.

---
+++ app/src/main/java/com/example/system/orgchatadmin/AddDepartment.java
@@ -0,0 +1,13 @@
+package com.example.system.orgchatadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDepartment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_department);
+    }
+}
