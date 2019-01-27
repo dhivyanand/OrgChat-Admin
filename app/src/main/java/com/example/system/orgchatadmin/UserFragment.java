4b51ffe2fbd8c3a3fd6db188c863985ebc1b052f
dhivyanand <dhivyananthan98@gmail.com>
2019-01-27 22:45:00 +0530

    Created fragment "UserFragment" and its corresponding resource files.

---
+++ app/src/main/java/com/example/system/orgchatadmin/UserFragment.java
@@ -0,0 +1,35 @@
+package com.example.system.orgchatadmin;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class UserFragment extends Fragment {
+
+
+
+    public UserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_user, container, false);
+        return root;
+
+    }
+
+}
