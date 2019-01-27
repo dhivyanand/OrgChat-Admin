e275dd6a7da95d080a9d5689e28066b1e8286d79
dhivyanand <dhivyananthan98@gmail.com>
2019-01-27 22:16:07 +0530

    Initialised the layout attributes of "AddDepartment" activity and added listeners.

--- app/src/main/java/com/example/system/orgchatadmin/AddDepartment.java
+++ app/src/main/java/com/example/system/orgchatadmin/AddDepartment.java
@@ -1,7 +1,9 @@
 package com.example.system.orgchatadmin;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -9,7 +11,7 @@ import android.widget.ListView;
 
 public class AddDepartment extends AppCompatActivity {
 
-    Button done;
+    Button cancel , done;
     EditText dept , sub_dept;
     ImageButton verify , add;
     ListView list;
@@ -18,5 +20,42 @@ public class AddDepartment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_department);
+
+        cancel = (Button)findViewById(R.id.cancel);
+        done = (Button)findViewById(R.id.done);
+        dept = (EditText)findViewById(R.id.dept);
+        sub_dept = (EditText)findViewById(R.id.sub_dept);
+        verify = (ImageButton)findViewById(R.id.verify);
+        add = (ImageButton)findViewById(R.id.add_sub_dept);
+        list = (ListView)findViewById(R.id.list);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
