795ddfaef54b92d4fd93f0f123deefb751d92863
dhivyanand <dhivyananthan98@gmail.com>
2019-01-27 20:45:44 +0530

    Implemented functions on Department Fragment.

--- app/src/main/java/com/example/system/orgchatadmin/DepartmentFragment.java
+++ app/src/main/java/com/example/system/orgchatadmin/DepartmentFragment.java
@@ -5,71 +5,45 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class DepartmentFragment extends Fragment {
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public DepartmentFragment() {
         // Required empty public constructor
     }
 
-    public static DepartmentFragment newInstance(String param1, String param2) {
-        DepartmentFragment fragment = new DepartmentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_department, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_department, container, false);
+
+        ListView dept = (ListView)root.findViewById(R.id.dept_list);
+
+        Button add = (Button)root.findViewById(R.id.add_dept);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getActivity(), "Button Clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        return root;
     }
 
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
-    }
 }
