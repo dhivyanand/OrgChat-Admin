c6e9445f6bd02ce233e2cdb1717a4a366ac41650
dhivyanand <dhivyananthan98@gmail.com>
2019-01-27 22:09:56 +0530

    Declared the layout attributes of "AddDepartment" activity.

--- app/src/main/java/com/example/system/orgchatadmin/AddDepartment.java
+++ app/src/main/java/com/example/system/orgchatadmin/AddDepartment.java
@@ -2,8 +2,17 @@ package com.example.system.orgchatadmin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class AddDepartment extends AppCompatActivity {
+
+    Button done;
+    EditText dept , sub_dept;
+    ImageButton verify , add;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
