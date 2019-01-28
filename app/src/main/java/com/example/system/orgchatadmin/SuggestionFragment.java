54d965742f923bea0f051d3c719ade8095645019
dhivyanand <dhivyananthan98@gmail.com>
2019-01-28 09:53:04 +0530

    Placed a listview in the suggestion fragment layout.

--- app/src/main/java/com/example/system/orgchatadmin/SuggestionFragment.java
+++ app/src/main/java/com/example/system/orgchatadmin/SuggestionFragment.java
@@ -24,8 +24,10 @@ public class SuggestionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_suggestion, container, false);
+        View root = inflater.inflate(R.layout.fragment_suggestion, container, false);
+
+        return root;
+
     }
 
 }
