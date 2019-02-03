a3bf0a14d4add34c006989b353c424139dfb3fe8
dhivyanand <dhivyananthan98@gmail.com>
2019-02-03 19:36:46 +0530

    Developed Sub Department Fragment and its resource file.

--- app/src/main/java/com/example/system/orgchatadmin/SubDepartmentFragment.java
+++ app/src/main/java/com/example/system/orgchatadmin/SubDepartmentFragment.java
@@ -1,15 +1,27 @@
 package com.example.system.orgchatadmin;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SubDepartmentFragment extends Fragment {
+
+    View root;
+    ListView dept;
+    Button add;
+    DepartmentListAdapter adapter;
+    Map<String,String> content;
 
     public SubDepartmentFragment() {
         // Required empty public constructor
@@ -24,8 +36,24 @@ public class SubDepartmentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sub_department, container, false);
+        root = inflater.inflate(R.layout.fragment_sub_department, container, false);
+
+        dept = (ListView)root.findViewById(R.id.dept_list);
+        add = (Button)root.findViewById(R.id.add_dept);
+        content = new HashMap<String,String>();
+
+        adapter = new DepartmentListAdapter(getContext(),content);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getActivity(),AddDepartment.class));
+
+            }
+        });
+
+        return root;
     }
 
 }
