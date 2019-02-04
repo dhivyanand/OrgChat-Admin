264de49c4e31da5fce0313e13fb6b9c177b5edb8
dhivyanand <dhivyananthan98@gmail.com>
2019-02-04 11:31:40 +0530

    Placed list view on both swipe layouts.

--- app/src/main/java/com/example/system/orgchatadmin/MainActivity.java
+++ app/src/main/java/com/example/system/orgchatadmin/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //startActivity(new Intent(MainActivity.this,home.class));
-        startActivity(new Intent(MainActivity.this,SubDepartmentMessageActivity.class));
+        startActivity(new Intent(MainActivity.this,home.class));
+        //startActivity(new Intent(MainActivity.this,SubDepartmentMessageActivity.class));
 
     }
 }
