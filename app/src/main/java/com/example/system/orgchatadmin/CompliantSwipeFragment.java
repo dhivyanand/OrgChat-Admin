5a48281108553031a5fff0e8bac0f4e4f223d0b1
dhivyanand <dhivyananthan98@gmail.com>
2019-02-04 10:28:59 +0530

    Created Fragment "CompliantSwipeFragment" and its resource file.

---
+++ app/src/main/java/com/example/system/orgchatadmin/CompliantSwipeFragment.java
@@ -0,0 +1,30 @@
+package com.example.system.orgchatadmin;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CompliantSwipeFragment extends Fragment {
+
+    public CompliantSwipeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_compliant_swipe, container, false);
+    }
+
+}
