bdd25e57b8b6430c0164ae16deaa32ecbe3b39d3
dhivyanand <dhivyananthan98@gmail.com>
2019-02-04 15:25:24 +0530

    Added icon "ic_attachment".

--- app/src/main/java/com/example/system/orgchatadmin/AttachmentAdapter.java
+++ app/src/main/java/com/example/system/orgchatadmin/AttachmentAdapter.java
@@ -1,13 +1,45 @@
 package com.example.system.orgchatadmin;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.io.File;
+import java.net.FileNameMap;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class AttachmentAdapter extends BaseAdapter {
+
+    Context c;
+    ArrayList<Uri> attachment;
+
+    private Bitmap thumbnailFromPath(Uri uri){
+
+        Bitmap thumbImage = ThumbnailUtils.extractThumbnail(
+                BitmapFactory.decodeFile(uri.getPath()),
+                128,
+                128);
+
+        return thumbImage;
+    }
+
+    AttachmentAdapter(Context c, ArrayList<Uri> attachment){
+
+        this.c = c;
+        this.attachment = attachment;
+
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return attachment.size();
     }
 
     @Override
@@ -22,6 +54,7 @@ public class AttachmentAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+
         return null;
     }
 }
--- app/src/main/java/com/example/system/orgchatadmin/NewCircular.java
+++ app/src/main/java/com/example/system/orgchatadmin/NewCircular.java
@@ -8,6 +8,7 @@ import android.widget.GridView;
 
 public class NewCircular extends AppCompatActivity {
 
+    AttachmentAdapter adapter;
     Button post;
     EditText title,description;
     GridView attachment;
