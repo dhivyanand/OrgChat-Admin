a133c1fb9a60e21eb47ae5aa13f9b20f3dac8173
dhivyanand <dhivyananthan98@gmail.com>
2019-02-04 14:16:53 +0530

    Declared and initialised layout variables in new circular activity layout.

--- app/src/main/java/com/example/system/orgchatadmin/NewCircular.java
+++ app/src/main/java/com/example/system/orgchatadmin/NewCircular.java
@@ -2,12 +2,25 @@ package com.example.system.orgchatadmin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
 
 public class NewCircular extends AppCompatActivity {
+
+    Button post;
+    EditText title,description;
+    GridView attachment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_circular);
+
+        post = (Button)findViewById(R.id.post);
+        title = (EditText)findViewById(R.id.circular_title);
+        description = (EditText)findViewById(R.id.circular_description);
+        attachment = (GridView)findViewById(R.id.attachment_list);
+
     }
 }
