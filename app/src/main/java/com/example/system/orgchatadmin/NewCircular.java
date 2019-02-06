845d9a70a234bb2534d229660f7c91ca3e1e9be9
dhivyanand <dhivyananthan98@gmail.com>
2019-02-06 12:30:44 +0530

    Created function "performFileSearch".

--- app/src/main/java/com/example/system/orgchatadmin/NewCircular.java
+++ app/src/main/java/com/example/system/orgchatadmin/NewCircular.java
@@ -1,5 +1,6 @@
 package com.example.system.orgchatadmin;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -25,10 +27,32 @@ public class NewCircular extends AppCompatActivity {
     AttachmentAdapter adapter;
     Button post;
     EditText title,description;
+    ImageButton add;
     ListView attachment;
     ArrayList<Bitmap> thumbnail;
     ArrayList<Boolean> is_video;
     ArrayList<String> path;
+
+    int READ_REQUEST_CODE = 42;
+
+    private void performFileSearch() {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+        // To search for all documents available via installed storage providers,
+        // it would be "*/*".
+        intent.setType("image/*");
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
 
     private boolean notEmpty(ArrayList<Bitmap> thumbnail, ArrayList<Boolean> is_video, ArrayList<String> path){
 
@@ -65,6 +89,7 @@ public class NewCircular extends AppCompatActivity {
         title = (EditText)findViewById(R.id.circular_title);
         description = (EditText)findViewById(R.id.circular_description);
         attachment = (ListView)findViewById(R.id.attachment_list);
+        add = (ImageButton)findViewById(R.id.add);
 
         thumbnail = new ArrayList<Bitmap>();
         is_video = new ArrayList<Boolean>();
@@ -75,6 +100,15 @@ public class NewCircular extends AppCompatActivity {
         attachment.setAdapter(adapter);
 
         adapter.notifyDataSetChanged();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
 
     }
 }
