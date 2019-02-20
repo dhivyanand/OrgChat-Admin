f8dec75eda445cee211696516f6c1198f5365db9
dhivyanand <dhivyananthan98@gmail.com>
2019-02-20 12:35:23 +0530

    Created receiver "NetworkStateReceiver".

---
+++ app/src/main/java/com/example/system/orgchatadmin/Receivers/NetworkStateReceiver.java
@@ -0,0 +1,15 @@
+package com.example.system.orgchatadmin.Receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetworkStateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
